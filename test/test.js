d0b691222014ad3e78decee11deac946ce973c62
Thanh An <thanhan7914@gmail.com>
2020-01-07 09:58:14 +0700

    config

--- test/test.js
+++ test/test.js
@@ -5,10 +5,12 @@ const PureRequest = require('../lib/base/request');
 
 console.log(DIContainer);
 let h = DIContainer.resolve('app/http/controllers/HomeController');
-const HomeController = require('../app/http/controllers/HomeController');
+const HomeController = require('../app/http/controllers/home-controller');
 console.log(h instanceof HomeController);
 
 console.log(require('../lib/config')('app.port'))
+console.log(require('../lib/config')('plugin.express-session'));
+
 
 const { trans, getLocale } = require('../lib/lang');
 console.log(trans('message.error_404'));
